692fceae0239a32b66cbeaf71af415620d1877d9
pngu0054 <pngu0054@student.monash.edu>
2024-05-29 15:57:16 +1000

    add VolcanoCard methods

--- sprint3-implementation/core/src/com/mygdx/game/Board/VolcanoCard.java
+++ sprint3-implementation/core/src/com/mygdx/game/Board/VolcanoCard.java
@@ -1,4 +1,24 @@
 package com.mygdx.game.Board;
 
+import com.badlogic.gdx.utils.Array;
+import com.mygdx.game.ChitCards.AnimalType;
+
 public class VolcanoCard {
+    private final Array<AnimalType> types = new Array<>();
+
+    public VolcanoCard(AnimalType... types) {
+        for (AnimalType boardSlot : types) {
+            this.types.add(boardSlot);
+        }
+    }
+
+    public Array<AnimalType> getTypes() {
+        if (Math.random() < 0.5) {
+            this.types.reverse();
+            return this.types;
+        } else {
+            return this.types;
+        }
+    }
 }
+
