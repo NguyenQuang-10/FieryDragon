f50a0d289aae9894ecc5692090b3909efd9f3ba0
pngu0054 <pngu0054@student.monash.edu>
2024-05-30 16:50:06 +1000

    add loading functionality for ChitCardManager

--- sprint3-implementation/core/src/com/mygdx/game/ChitCards/ChitCardManager.java
+++ sprint3-implementation/core/src/com/mygdx/game/ChitCards/ChitCardManager.java
@@ -2,13 +2,17 @@ package com.mygdx.game.ChitCards;
 
 import com.mygdx.game.Board.Board;
 import com.mygdx.game.Board.Player;
+import org.yaml.snakeyaml.Yaml;
 
-import java.util.ArrayList;
-import java.util.Collections;
+import java.io.InputStream;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.*;
 
 // A class that contains a collection of all chit card in the game
 // allowing it to manage turns
 public class ChitCardManager implements ITurnManager {
+    private final Map<String, AnimalType> animalTypeMap;
     // players in the game
     protected Player[] players;
 
@@ -17,11 +21,15 @@ public class ChitCardManager implements ITurnManager {
 
     // the index of the player whose turn it is in this.players
     int activePlayerIndex; // index of players whose turn belong to
+    String mode;
+    HashMap<String, List<String>> yamlData;
 
     // Constructor
-    public ChitCardManager(Player[] players){
+    public ChitCardManager(Player[] players, String mode, Map<String, AnimalType> animalTypeMap){
         this.players = players;
         this.activePlayerIndex = 0;
+        this.mode = mode;
+        this.animalTypeMap = animalTypeMap;
     }
 
     // Generate 16 chit card that follows the base game rules
@@ -30,23 +38,52 @@ public class ChitCardManager implements ITurnManager {
      */
     public void generateChitCards(Board board) {
         ArrayList<ChitCard> cards = new ArrayList<>();
-        AnimalType[] animalTypes =  AnimalType.values();
+        if (mode.equals("default")) {
+            // create an array of animalTypes
+            AnimalType[] animalTypes = animalTypeMap.values().toArray(new AnimalType[0]);
 
-        for (AnimalType animalType : animalTypes) {
-            if (animalType != AnimalType.PIRATE_DRAGON) {
-                for (int aCount = 1; aCount <= 3; aCount++) {
-                    ChitCard card = new RegularChitCard(animalType, aCount, board, this);
-                    cards.add(card);
+            // generate the ChitCards sequentially and add regular ChitCard to the board
+            for (AnimalType animalType : animalTypes) {
+                if (animalType != AnimalType.PIRATE_DRAGON) {
+                    for (int aCount = 1; aCount <= 3; aCount++) {
+                        ChitCard card = new RegularChitCard(animalType, aCount, board, this);
+                        cards.add(card);
+                    }
                 }
             }
+
+            // generate the PIRATE_DRAGON ChitCard
+            for (int i = 1; i <= 2; i++) {
+                cards.add(new PirateChitCard(i, board, this));
+                cards.add(new PirateChitCard(i, board, this));
+            }
+            // shuffle the generated ChitCards
+            Collections.shuffle(cards);
+        } else if (mode.equals("custom")) {
+            try {
+                // load yaml file into yamlData
+                Yaml yaml = new Yaml();
+                InputStream inputStream = Files.newInputStream(Paths.get("save_file.yaml"));
+                yamlData = yaml.load(inputStream);
+                inputStream.close();
+            } catch (Exception e) {
+                System.out.println(e.getMessage());
+            }
+
+            // load the data of ChitCard in yaml file
+            List<String> chitCardTypeList = yamlData.get("chitCardType");
+            List<String> chitCardNumberList = yamlData.get("chitCardNumber");
+            for (int i = 0; i < chitCardTypeList.size(); i++) {
+                if (chitCardTypeList.get(i).equals("PIRATE_DRAGON")) {
+                    cards.add(new PirateChitCard(Integer.parseInt(chitCardNumberList.get(i)), board, this));
+                } else {
+                    cards.add(new RegularChitCard(animalTypeMap.get(chitCardTypeList.get(i)), Integer.parseInt(chitCardNumberList.get(i)), board, this));
+                }
+            }
+
         }
 
-        for (int i = 1; i <= 2; i++) {
-            cards.add(new PirateChitCard(i, board, this));
-            cards.add(new PirateChitCard(i, board, this));
-        }
 
-        Collections.shuffle(cards);
         chitCards = new ChitCard[16];
         chitCards = cards.toArray(chitCards);
     }
