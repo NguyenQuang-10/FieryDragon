4c0972333947f9a5dd270b7763ed92146827ca77
pngu0054 <pngu0054@student.monash.edu>
2024-05-30 23:10:20 +1000

    add new save() method for ITurnManager interface

--- sprint3-implementation/core/src/com/mygdx/game/ChitCards/ITurnManager.java
+++ sprint3-implementation/core/src/com/mygdx/game/ChitCards/ITurnManager.java
@@ -2,10 +2,15 @@ package com.mygdx.game.ChitCards;
 
 import com.mygdx.game.Board.Player;
 
+import java.util.List;
+import java.util.Map;
+
 // Interface that describe a class that can manage turn for the game
 public interface ITurnManager {
     // end this turn and move on to the turn for the next player
     public void endTurn();
     // get the player whose turn it is
     public Player getActivePlayer();
+
+    Map<String, List<String>> save();
 }
